83f2f332c7dce5167709ebc42ca95e8ebe9ff469
guillermau <guillaume@bourdat.eu>
2017-08-01 21:55:20 +0200

    feat: integration card user in user details view

--- src/app/views/users/user-details/user-details.component.ts
+++ src/app/views/users/user-details/user-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../../../services/users.service';
+import { User } from "../../../models/user";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-details',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailsComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  userId: number;
+
+  constructor(private userService: UsersService,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      this.userId = params['userId'];
+      this.user = this.userService.show(this.userId);
+    })
   }
 
 }
