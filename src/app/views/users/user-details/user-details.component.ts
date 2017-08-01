ce64a85a4111105d5ce0c796eed04f16a6f8210b
guillermau <guillaume@bourdat.eu>
2017-08-01 22:46:03 +0200

    feat: integration order list comp in user details view

--- src/app/views/users/user-details/user-details.component.ts
+++ src/app/views/users/user-details/user-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../../../services/users.service';
+import { OrdersService } from '../../../services/orders.service';
 import { User } from "../../../models/user";
+import { Order } from "../../../models/order";
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -11,15 +13,18 @@ import { ActivatedRoute } from '@angular/router';
 export class UserDetailsComponent implements OnInit {
 
   user: User;
+  orders: Order[];
   userId: number;
 
   constructor(private userService: UsersService,
+              private ordersService: OrdersService,
               private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.activatedRoute.params.subscribe(params => {
       this.userId = params['userId'];
       this.user = this.userService.show(this.userId);
+      this.orders = this.ordersService.listByClient(this.userId);
     })
   }
 
