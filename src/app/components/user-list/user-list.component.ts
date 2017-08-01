efb3a5f23dee136c808181114cbc5d500e8788d7
guillermau <guillaume@bourdat.eu>
2017-08-01 22:46:23 +0200

    style: remove unused import

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {User} from "../../models/user";
 
 @Component({
